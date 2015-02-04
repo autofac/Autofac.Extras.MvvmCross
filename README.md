c74bdd2862d2faecce1ffe1f4b9bdb7e44c2f627
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-04 22:22:33 +1000

    Added solution and build configuration.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Autofac.Extras.MvvmCross
 ========================
 
 MvvmCross integration for Autofac IoC
+
+[![Build status](https://ci.appveyor.com/api/projects/status/bg69gsksk4nxawso?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-mvvmcross)
--- src/Autofac.Extras.MvvmCross/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.MvvmCross/Properties/AssemblyInfo.cs
@@ -1,13 +1,24 @@
 using System;
 using System.Reflection;
+using System.Resources;
 
 // MvvmCross integration is not marked with APTCA because the MvvmCross
 // framework is all SecurityCritical by default.
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Autofac.Extras.MvvmCross")]
-
-// MvvmCross is not marked CLS compliant.
-[assembly: CLSCompliant(false)]
-
+[assembly: CLSCompliant(false)] // MvvmCross is not marked CLS compliant.
 [assembly: ComVisible(false)]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("MvvmCross integration for Autofac IoC")]
